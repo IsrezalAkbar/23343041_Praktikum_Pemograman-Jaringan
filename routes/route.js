168427de4a7149699842ceb82cbb0dec172556e5
Isrezal Akbar <isrezalakbar03@gmail.com>
2025-12-24 00:43:28 +0700

    Delete routes directory

--- routes/route.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-
-const { signup, getBill } = require("../controller/appController.js");
-
-router.post("/user/signup", signup);
-router.post("/product/bill", getBill);
-
-module.exports = router;
